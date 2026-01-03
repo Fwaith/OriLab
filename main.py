b762d2d1b3d2fa4f5c06dfc5fbb1f420d7d8277f
Fwaith <imanasyik@gmail.com>
2026-01-03 03:36:49 +0000

    Revolving RGB Cube

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
 import glfw
+import glm
+import math
 import numpy as np
 from OpenGL.GL import *
 
@@ -10,11 +12,37 @@ import input
 def main():
     window=display.window()
     render.object()
+
+    model_loc = glGetUniformLocation(render.shaderProgram, "model")
+    view_loc = glGetUniformLocation(render.shaderProgram, "view") 
+    projection_loc = glGetUniformLocation(render.shaderProgram, "projection")
+
+    glEnable(GL_DEPTH_TEST)
     while not glfw.window_should_close(window):
         input.processInput(window)
        
-        glClear(GL_COLOR_BUFFER_BIT)
-        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4)
+        glClearColor(1.0, 1.0, 1.0, 1.0)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        glUseProgram(render.shaderProgram)
+
+        timeValue = glfw.get_time()
+        greenValue = (math.sin(timeValue))
+        vertexColorLocation = glGetUniformLocation(render.shaderProgram, "ourColor")
+
+        model = glm.mat4(1.0)
+        model = glm.rotate(model, (glfw.get_time() * glm.radians(50.0)), glm.vec3(0.5, 1.0, 0.0))
+        view = glm.lookAt(glm.vec3(1.0, 1.0, 2.0), glm.vec3(0.0, 0.0, 0.0), glm.vec3(0.0, 1.0, 0.0))
+        projection = glm.perspective(glm.radians(60.0), 600.0 / 600.0, 0.1, 100.0)
+        glUseProgram(render.shaderProgram)
+        glUniform4f(vertexColorLocation, 0.0, greenValue, 0.0, 1.0)
+
+        glUniformMatrix4fv(model_loc, 1, GL_FALSE, glm.value_ptr(model))
+        glUniformMatrix4fv(view_loc, 1, GL_FALSE, glm.value_ptr(view))
+        glUniformMatrix4fv(projection_loc, 1, GL_FALSE, glm.value_ptr(projection))
+        
+        glBindVertexArray(render.VAO)
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, None)
         glfw.swap_buffers(window)
         glfw.swap_interval(1)
         glfw.poll_events()
--- render.py
+++ render.py
@@ -1,72 +1,126 @@
 import glfw
+import glm
+import math
 import numpy as np
 from OpenGL.GL import *
+from PIL import Image
+
+VAO = None
+shaderProgram = None
 
 def object():
-    program = glCreateProgram()
-    vertex_shader = glCreateShader(GL_VERTEX_SHADER)
-    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER)
-
-    vertex_shader_source = """
-        attribute vec2 position;
-        attribute vec4 color;
-        varying vec4 v_color;
-        void main() { 
-            gl_Position = vec4(position, 0.0, 1.0);
-            v_color = color;
-        } """
-
-    fragment_shader_source = """
-        varying vec4 v_color;
-        void main() { 
-            gl_FragColor = v_color;
-        } """
-
-    glShaderSource(vertex_shader, vertex_shader_source)
-    glShaderSource(fragment_shader, fragment_shader_source)
-
-    glCompileShader(vertex_shader)
-    if not glGetShaderiv(vertex_shader, GL_COMPILE_STATUS):
-        error = glGetShaderInfoLog(vertex_shader).decode()
-        print(error)
-        raise RuntimeError("Vertex shader compilation error")
+    global VAO, shaderProgram    
     
-    glCompileShader(fragment_shader)
-    if not glGetShaderiv(fragment_shader, GL_COMPILE_STATUS):
-        error = glGetShaderInfoLog(fragment_shader).decode()
-        print(error)
-        raise RuntimeError("Fragment shader compilation error")
+    vertices = np.array([
+        -0.5, 0.5, 0.5,  1.0, 0.0, 0.0  ,0.0, 1.0, #top left
+        0.5, 0.5, 0.5,  0.0, 1.0, 0.0  ,1.0, 1.0, #top right
+        -0.5, -0.5, 0.5,  0.0, 0.0, 1.0  ,0.0, 0.0, #bottom left
+        0.5, -0.5, 0.5,  1.0, 1.0, 0.0  ,1.0, 0.0, #bottom right
 
-    glAttachShader(program, vertex_shader)
-    glAttachShader(program, fragment_shader)
-    glLinkProgram(program)
+        -0.5, 0.5, -0.5,  1.0, 0.0, 0.0  ,0.0, 1.0, #top left back
+        0.5, 0.5, -0.5,  0.0, 1.0, 0.0  ,1.0, 1.0, #top right back
+        -0.5, -0.5, -0.5,  0.0, 0.0, 1.0  ,0.0, 0.0, #bottom left back
+        0.5, -0.5, -0.5,  1.0, 1.0, 0.0  ,1.0, 0.0 #bottom right back
+    ], dtype=np.float32)
 
-    glDetachShader(program, vertex_shader)
-    glDetachShader(program, fragment_shader)
+    indices = np.array([
+        0, 1, 2,  2, 1, 3, #front
+        4, 5, 6,  5, 6, 7, #back
+        0, 4, 6,  0, 6, 2, #left
+        1, 5, 3,  5, 3, 7, #right
+        0, 4, 5,  5, 0, 1, #top
+        2, 7, 6,  7, 2, 3  #bottom
+    ], dtype=np.uint32)
 
-    glUseProgram(program)
+    VAO = glGenVertexArrays(1)
+    glBindVertexArray(VAO)
 
-    data = np.zeros(4, [("position", np.float32, 2),
-                        ("color", np.float32, 4)])
-    data['position'] = [(-1,+1), (+1,+1), (-1,-1), (+1,-1)]
-    data['color'] = [(0,1,0,1), (1,1,0,1), (1,0,0,1), (0,0,1,1)]
+    VBO = glGenBuffers(1)
+    glBindBuffer(GL_ARRAY_BUFFER, VBO)
+    glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
+    
+    EBO = glGenBuffers(1)
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO)
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.nbytes, indices, GL_STATIC_DRAW)
 
-    buffer = glGenBuffers(1)
-    glBindBuffer(GL_ARRAY_BUFFER, buffer)
+    stride = 8 * 4
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, None)
+    glEnableVertexAttribArray(0)
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, stride, ctypes.c_void_p(12))
+    glEnableVertexAttribArray(1)
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, stride, ctypes.c_void_p(24))
+    glEnableVertexAttribArray(2)
 
-    stride = data.strides[0]
-    offset = ctypes.c_void_p(0)
-    location = glGetAttribLocation(program, "position")
-    glEnableVertexAttribArray(location)
-    glBindBuffer(GL_ARRAY_BUFFER, buffer)
-    glVertexAttribPointer(location, 2, GL_FLOAT, False, stride, offset)
+    image = Image.open("metal.jpg")
+    width, height = image.size
+    """nrChannels = len(image.getbands())"""
+    data = np.array(image)
 
-    offset = ctypes.c_void_p(data.dtype["position"].itemsize)
-    location = glGetAttribLocation(program, "color")
-    glEnableVertexAttribArray(location)
-    glBindBuffer(GL_ARRAY_BUFFER, buffer)
-    glVertexAttribPointer(location, 4, GL_FLOAT, False, stride, offset)
+    texture = glGenTextures(1)
+    glBindTexture(GL_TEXTURE_2D, texture)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data)
+    glGenerateMipmap(GL_TEXTURE_2D)
 
-    location = glGetUniformLocation(program, "color")
-    glUniform4f(location, 0.0, 0.0, 1.0, 1.0)
-    glBufferData(GL_ARRAY_BUFFER, data.nbytes, data, GL_DYNAMIC_DRAW)
+    vertexShaderSource = """
+        #version 460 core
+        layout (location = 0) in vec3 aPos;
+        layout (location = 1) in vec3 aColor;
+        layout (location = 2) in vec2 aTexCoord;
+
+        out vec2 TexCoord;
+        out vec3 Pos;
+
+        uniform mat4 model;
+        uniform mat4 view;
+        uniform mat4 projection;
+
+        void main() {
+            gl_Position = projection * view * model * vec4(aPos.x, aPos.y, aPos.z, 1.0);
+            Pos = aPos;
+            TexCoord = aTexCoord;
+        } """
+
+    fragmentShaderSource = """
+        #version 460 core
+        out vec4 FragColor;
+
+        in vec2 TexCoord;
+        in vec3 Pos;
+
+        uniform vec4 ourColor;
+        uniform sampler2D ourTexture;
+
+        void main() {
+            FragColor = texture(ourTexture, TexCoord) * vec4(Pos.x, ourColor.g, Pos.z, 1.0);
+        } """
+
+    vertexShader = glCreateShader(GL_VERTEX_SHADER)
+    glShaderSource(vertexShader,vertexShaderSource)
+    glCompileShader(vertexShader)
+
+    if not glGetShaderiv(vertexShader, GL_COMPILE_STATUS):
+        error = glGetShaderInfoLog(vertexShader).decode()
+        print("Vertex shader error:", error)
+        raise RuntimeError("Vertex shader compilation failed")
+
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER)
+    glShaderSource(fragmentShader, fragmentShaderSource)
+    glCompileShader(fragmentShader)
+
+    if not glGetShaderiv(fragmentShader, GL_COMPILE_STATUS):
+        error = glGetShaderInfoLog(fragmentShader).decode()
+        print("Fragment shader error:", error)
+        raise RuntimeError("Fragment shader compilation failed")
+
+    shaderProgram = glCreateProgram()
+    glAttachShader(shaderProgram, vertexShader)
+    glAttachShader(shaderProgram, fragmentShader)
+    glLinkProgram(shaderProgram)
+    
+    if not glGetProgramiv(shaderProgram, GL_LINK_STATUS):
+        error = glGetProgramInfoLog(shaderProgram).decode()
+        print("Program linking error:", error)
+        raise RuntimeError("Shader program linking failed")
+
+    glDeleteShader(vertexShader)
+    glDeleteShader(fragmentShader)
